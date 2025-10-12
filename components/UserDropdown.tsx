74fb1eca0a9c128ac4647101d6f7af84d91ebd71
FuadAbdullah97 <ifuadabdullah@gmail.com>
2025-10-12 23:30:15 +0600

    implement Header

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -32,12 +32,32 @@ function UserDropdown() {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuItem>
+                    <div className={"flex relative items-center gap-3 py-2 cursor-pointer"}>
+
+                            <Avatar className={"h-8 w-8"}>
+                                {/*<AvatarImage src="https://github.com/shadcn.png" />*/}
+                                <AvatarFallback className={"bg-yellow-500 text-yellow-900 text-sm font-bold"}>
+                                    {user.name[0]}
+                                </AvatarFallback>
+                            </Avatar>
+
+                        <div className={"flex flex-col gap-0"}>
+                            <p className={"font-medium "}>{user.name}</p>
+                            <p className={"font-light"}>{user.email}</p>
+                        </div>
+
+                    </div>
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator className={"bg-gray-600"} />
+                <DropdownMenuItem className={"text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer"}>Logout</DropdownMenuItem>
+                <div className={"lg:hidden md:flex-col"}>
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </div>
             </DropdownMenuContent>
         </DropdownMenu>
     )
